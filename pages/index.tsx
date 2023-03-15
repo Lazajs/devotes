0b0df3257fdf0caefc3a959a0f9465ed2a25e84d
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-03-14 21:04:22 -0300

    fix: logout icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,11 @@ import type { User } from '@/types';
 
 const {Out} = Icons
 
-export default function Home({ user }: {user?: User}) {
+export default function Home({ user }: {user: User}) {
   const router = useRouter()
-  
   console.log(user)
+  
   const handleLogout = () => {
-    console.log('CLICK')
     fetch('/api/auth/logout').then(res => {
       router.push('/auth/register')
     })
@@ -20,7 +19,7 @@ export default function Home({ user }: {user?: User}) {
 
   return (
     <MainLayout>
-      {user && user?.name ? <Out className='cursor-pointer w-[50px] h-[50px] block m-auto z-10' onClick={handleLogout} /> : ''}
+      <span className='cursor-pointer w-[50px] h-[50px] block m-auto pt-big z-10' onClick={handleLogout}><Out className='w-[50px] h-[50px]' /></span>
     </MainLayout>
   )
 }
@@ -28,22 +27,19 @@ export default function Home({ user }: {user?: User}) {
 export const getServerSideProps = withIronSessionSsr(
   async function getServerSideProps({req}) {
     const user = req.session.user
-    console.log(user)
     
-    if (!user || !user?.name) {
-      return {
-        redirect: {
-          destination: '/auth/register',
-          permanent: false
-        }
-      }
-    }
-
-    else if (user) {
+    if (user && user?.name) {
       return {
         props: {
           user: user
         }
+      }
+    }
+    
+    return {
+      redirect: {
+        destination: '/auth/login',
+        permanent: false
       }
     }
   }, config
